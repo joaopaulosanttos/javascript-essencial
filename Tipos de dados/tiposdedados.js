031d76ffd6c359dbc3ccd3208d86c9c013d2bac1
jattasantosdev <contatosantosjp@gmail.com>
2022-06-22 10:37:50 -0300

    adicionando tipos de dados

---
+++ Tipos de dados/tiposdedados.js
@@ -0,0 +1,44 @@
+// Tipos de dados 
+
+// string
+const name = 'João Paulo';
+console.log(name);
+console.log(typeof name);
+
+// number
+const shirtsQty = 10;
+console.log(shirtsQty);
+console.log(typeof shirtsQty);
+
+// boolean
+const isApproved = false; // true ou false
+console.log(isApproved);
+console.log(typeof isApproved);
+
+// null 
+let surname = null;
+console.log(surname);
+console.log(typeof surname); // object 
+
+surname = 'João Paulo';
+console.log(surname);
+console.log(typeof surname);
+
+// undefined
+let age;
+console.log(age);
+console.log(typeof age);
+
+age = 24;
+console.log(age);
+console.log(typeof age);
+
+// array
+const languages = ['Javascript', 'PHP', 'Java', 'C#'];
+console.log(languages);
+console.log(typeof languages);
+
+// object 
+const user = {email: 'jpcontato@gmail.com', password: 'teste123', age: 25}; // object literals 
+console.log(user);
+console.log(typeof user);
---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello World Javascript!");
+
+// Console 
+console.error("Este é um erro!");
+console.warn("Este é um aviso!");
+console.log("Esta é uma mensagem!");
+
+// Variáveis
+var x = 10;
+// mais modernas 
+let y = 20;
+const z = 30; // O valor de const não pode mudar
+
+if(true) {
+    // variável do escopo
+    let y = 55;
+}
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+x = 15;
+
+console.log(x);
+
